a1904cd2b78326e59530130641bd0d8a13d035a7
moru348 <moru348@gmail.com>
2021-09-06 15:33:22 +0900

    update

--- src/main/java/dev/moru3/tickratechanger/TickrateChanger.kt
+++ src/main/java/dev/moru3/tickratechanger/TickrateChanger.kt
@@ -6,8 +6,6 @@ import net.minecraft.client.Minecraft
 import net.minecraft.command.Commands
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.eventbus.api.SubscribeEvent
-import net.minecraftforge.fml.ExtensionPoint
-import net.minecraftforge.fml.ModLoadingContext
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent
 import kotlin.concurrent.thread
@@ -15,12 +13,12 @@ import kotlin.concurrent.thread
 @Mod("tickratechanger")
 class TickrateChanger {
 
-    var tickConfig: TickrateConfigScreen? = null
+    var tickConfig: TickrateConfigScrollContents? = null
 
     @SubscribeEvent
     fun onServerStarting(event: FMLServerStartingEvent) {
         SERVER = event.server
-        if(tickConfig==null) { tickConfig = TickrateConfigScreen() }
+        if(tickConfig==null) { tickConfig = TickrateConfigScrollContents() }
         event.server.commandManager.dispatcher.register(Commands.literal("tickratechanger").executes {
             thread {
                 Thread.sleep(2 * Data.clientTick.toLong())
