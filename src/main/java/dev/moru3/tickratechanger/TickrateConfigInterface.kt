43d01a880890f8cf64f6bd9fac6527d3f1d68be2
moru348 <moru348@gmail.com>
2021-09-08 00:55:12 +0900

    update

--- src/main/java/dev/moru3/tickratechanger/TickrateConfigInterface.kt
+++ src/main/java/dev/moru3/tickratechanger/TickrateConfigInterface.kt
@@ -2,14 +2,18 @@ package dev.moru3.tickratechanger
 
 import com.mojang.blaze3d.matrix.MatrixStack
 import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.gui.screen.inventory.InventoryScreen
 import net.minecraft.client.gui.widget.list.ExtendedList
+import net.minecraft.client.settings.KeyBinding
+import net.minecraft.inventory.container.Container
+import net.minecraftforge.client.event.InputEvent
 
 
 class TickrateConfigScrollContents: ExtendedList.AbstractListEntry<TickrateConfigScrollContents>() {
     override fun render(matrixStack: MatrixStack, index: Int, top: Int, left: Int, width: Int, height: Int, mouseX: Int, mouseY: Int, isMouseOver: Boolean, partialTicks: Float) {
-        TODO("Not yet implemented")
-    }
 
+    }
 
 }
 
